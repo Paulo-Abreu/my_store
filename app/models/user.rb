06f12ef00ec1b731c550d13271884c1d7c378109
Paulo-Abreu <paulorabreuxd@gmail.com>
2021-11-16 10:02:57 -0300

    creating models, and doing unique tests

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :products
+  enum user_type: [:customer, :salesman] 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
