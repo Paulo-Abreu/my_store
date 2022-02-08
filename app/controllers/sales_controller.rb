38b2b601e1adcc9ae164aa74657a7e0e9e9f1d81
Paulo-Abreu <paulorabreuxd@gmail.com>
2022-02-08 11:25:07 -0300

    remove item from stock at time of purchase

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,23 +2,15 @@
 
 # Controller to manage the pages about sales.
 class SalesController < ApplicationController
-  before_action :view_product, only: %i[checkout]
-
   def checkout
     @props = {
       data: {
-        product: @product,
+      stock: StockItem.all.map { |r| ActiveModel::SerializableResource.new(r) },
         user: current_user
       },
       component: {
         name: 'checkout_page'
       }
     }
-  end
-
-  private
-
-  def view_product
-    @product = Product.find(params[:id])
   end
 end
