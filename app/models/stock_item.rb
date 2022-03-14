40c43b1cbce87065e8000dea9516b1e7acd5f7ec
Paulo-Abreu <paulorabreuxd@gmail.com>
2022-03-14 12:10:54 -0300

    add error validation

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -3,4 +3,6 @@
 class StockItem < ApplicationRecord
   belongs_to :product
   has_many :stock_movements, through: :product
+  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than: -1}
 end
--- spec/controllers/api/v1/stocks_controller_spec.rb
+++ spec/controllers/api/v1/stocks_controller_spec.rb
@@ -19,10 +19,19 @@ RSpec.describe Api::V1::StocksController, type: :controller do
     let(:user){ create(:user) }
     let(:product){ create(:product, user: user) }
     let!(:stock_item){ create(:stock_item, product: product) }
-    
-    it 'update a stock_item' do
-      patch :update, params: { id: stock_item.id, stock_item: { quantity: stock_item.quantity } }
-      expect(response).to have_http_status(200)
+     
+    context 'when success' do
+      it 'update a stock_item' do
+        patch :update, params: { id: stock_item.id, stock_item: { quantity: stock_item.quantity } }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when error' do
+      it 'update a stock_item' do
+        patch :update, params: { id: stock_item.id, stock_item: { quantity: stock_item.quantity - 50 } }
+        expect(response).to have_http_status(422)
+      end
     end
   end
 end
